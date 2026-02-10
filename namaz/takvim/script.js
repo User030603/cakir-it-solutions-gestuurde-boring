96ee6b95103816658dfebf5221e227a0e80776ff
User030603 <biggamer3415@gmail.com>
2026-02-10 15:10:46 +0100

    Delete namaz/takvim/script.js

--- namaz/takvim/script.js
+++
@@ -1,5 +0,0 @@
-// script.js içindeki goToCalendar fonksiyonunda bu satırı değiştir:
-window.location.href = `takvim/index.html?id=${finalId}&name=${encodeURIComponent(finalName)}`;
-
-// VEYA daha şık görünsün dersen (Çoğu tarayıcı index.html'i otomatik anlar):
-window.location.href = `takvim/?id=${finalId}&name=${encodeURIComponent(finalName)}`;
